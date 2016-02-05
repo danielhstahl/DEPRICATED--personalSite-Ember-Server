8e8dadfca9d5c3091fc3f69e7823205b523ffd59
phillyfan1138 <danstahl1138@gmail.com>
2016-02-05 17:33:42 -0500

    working serving

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ var express = require('express');
 var bodyParser=require('body-parser');
 var handleYield=require('handleYield');
 var app = express();
+//var jade=require('jade');
 var server = http.createServer(app).listen(7000); //required fro socket.io
 var io = require('socket.io').listen(server);
 app.use(bodyParser.json());
 app.use(express.static('dist'));
+app.set('view engine', 'html');
 /*app.get('/', function (req, res) {
    res.render('index');
 });*/
 app.get('/', function(req, res){
-  res.render('dist/index.html');
+  res.render('dist/index');
 });
 var genericDataFunction=function(data, fullData, callback){
   data=data.split(/\\n/g);
